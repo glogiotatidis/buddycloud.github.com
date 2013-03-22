c1ae07c43b015ba627876bc7989076b16a36b787
Vítor Avelino <contact@vitoravelino.net>
2013-03-22 10:46:42 +0100

    Added screenshots transition and new layout proposal for features section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 $(function() {
-    $('.fixedHeader .container').append($('.primarySection header').clone());
 
-    $('.socialNetwork').waypoint(function(direction) {
-        if (direction === 'down') {
-            $('.fixedHeader').fadeIn('fast');
-        } else {
-            $('.fixedHeader').fadeOut('fast');
-        }
-    });
-})
+  // fixed top bar
+  $('.fixedHeader .container').append($('.primarySection header').clone());
+
+  $('.features').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('.fixedHeader').fadeIn('fast');
+    } else {
+      $('.fixedHeader').fadeOut('fast');
+    }
+  });
+
+  // screenshots transitions
+  (function() {
+    var $screenshots = $('.screenshots img'),
+        $head = $screenshots.first(),
+        $next = $head,
+        $current;
+
+    setInterval(function() {
+      $current = $next;
+      $next = $current.next();
+
+      if (!$next[0]) $next = $head;
+
+      $current.fadeOut();
+      $next.fadeIn();
+    }, 4000);
+  }());
+
+});
